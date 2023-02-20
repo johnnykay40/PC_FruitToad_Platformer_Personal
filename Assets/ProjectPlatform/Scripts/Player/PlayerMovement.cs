7a2bc54db9b4f68b990cf5ea968035345864333a
Johnny Colorado <johnnykay30@gmail.com>
2023-02-20 16:33:58 -0500

    DONE Animations for Jump and Run

--- Assets/ProjectPlatform/Scripts/Player/PlayerMovement.cs
+++ Assets/ProjectPlatform/Scripts/Player/PlayerMovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D playerRb;
 
     public SpriteRenderer spriteRenderer;
+    public Animator animator;
 
 
 
@@ -27,21 +28,35 @@ public class PlayerMovement : MonoBehaviour
         {
             playerRb.velocity = new Vector2(playerSpeed, playerRb.velocity.y);
             spriteRenderer.flipX = false;
+            animator.SetBool("isRunning", true);
         }
         
         else if (Input.GetKey("a"))
         {
             playerRb.velocity = new Vector2(-playerSpeed, playerRb.velocity.y);
             spriteRenderer.flipX = true;
+            animator.SetBool("isRunning", true);
         }
 
         else
         {
             playerRb.velocity = new Vector2(0, playerRb.velocity.y);
+            animator.SetBool("isRunning", false);
         }
         if (Input.GetKey("space") && PlayerCheckGround.isGrounded)
         {
-            playerRb.velocity = new Vector2(playerRb.velocity.x, playerJump);
+            playerRb.velocity = new Vector2(playerRb.velocity.x, playerJump);           
+        }
+
+        if (PlayerCheckGround.isGrounded == false)
+        {
+            animator.SetBool("isJumping", true);
+            animator.SetBool("isRunning", false);
+        } 
+        if (PlayerCheckGround.isGrounded == true)
+        {
+            animator.SetBool("isJumping", false);
+            
         }
 
         if (playerBetterJump)
